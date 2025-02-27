86be0b2c470a3154493e491e2deb58b771759459
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2025-02-27 15:27:29 +0000

    fix wording

--- wellnista-app/app/menu/page.tsx
+++ wellnista-app/app/menu/page.tsx
@@ -1,10 +1,10 @@
 export default function MenuScreen() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-4">Wellnista</h1>
+      <h2 className="text-3xl font-bold mb-4 text-blue-60">Wellnista</h2>
       {/* <p className="mb-6 text-gray-600">Choose your action below to get started.</p> */}
       <div className="flex flex-col gap-4">
-        ฟีเจอร์นี้อยู่ระหว่างการพัฒนา
+        ฟีเจอร์นี้อยู่ระหว่างการพัฒนา 2
       </div>
     </div>
   );
