4a444b9ddfb42eee42d9a3af5c4a06d3924db2be
Chaning <88159606+Ninggggggg07@users.noreply.github.com>
2025-03-06 14:19:39 +0000

    adding new screen

---
+++ wellnista-app/app/select/page.tsx
@@ -0,0 +1,11 @@
+export default function SelectScreen() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h2 className="text-3xl font-bold mb-4 text-blue-60">Wellnista</h2>
+        {/* <p className="mb-6 text-gray-600">Choose your action below to get started.</p> */}
+        <div className="flex flex-col gap-4">
+          Select Screen
+        </div>
+      </div>
+    );
+  }
