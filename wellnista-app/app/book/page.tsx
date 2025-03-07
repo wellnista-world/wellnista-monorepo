8a55b8a7078b1e2795426d8640febc821abcf958
omiiz9045 <kunatip.cha@gmail.com>
2025-03-07 11:25:36 +0000

    book update

--- wellnista-app/app/book/page.tsx
+++ wellnista-app/app/book/page.tsx
@@ -29,7 +29,9 @@ export default function InforDtx() {
     
     <div className="grid grid-cols-6 gap-4 md:place-content-center bg-secondary text-neutral font-garet ">
 
-      <div className="col-span-6 text-center text-3xl font-magnolia text-primary ">สมุดบันทึกน้ำตาล</div>
+      <div className="col-span-6 text-center text-3xl font-magnolia text-primary ">
+        สมุดบันทึกน้ำตาล
+      </div>
 
       <div className="col-span-4 text-2xl font-bold text-neutral">
         วัน - เวลา
@@ -46,14 +48,32 @@ export default function InforDtx() {
         มื้ออาหาร
       </div>
 
-      <div className=" col-start-1 col-end-3 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+      <div className="py-3 col-start-1 col-end-3 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
         เช้า
       </div>
-      <div className=" col-start-3 col-end-5 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+      <div className="py-3 col-start-3 col-end-5 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
         กลางวัน
       </div>
-      <div className=" col-start-5 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+      <div className="py-3 col-start-5 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
         เย็น
+      </div>
+
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        ก่อนอาหาร
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        หลังอาหาร
+      </div>
+
+      <div className="col-start-1 col-end-3 text-4xl content-center text-center font-bold text-neutral">
+        DTX
+      </div>
+      <div className="py-3 col-start-3 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input DTX
+      </div>
+
+      <div className="mt-20 py-3 col-start-2 col-end-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        บันทึก
       </div>
     
     </div>
