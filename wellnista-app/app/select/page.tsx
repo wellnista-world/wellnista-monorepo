479f504c3f8a8a9584f4eb81cec5d3e14ef7735e
Chaning <chanikankatti47@gmail.com>
2025-03-10 15:21:52 +0700

    Done select page

--- wellnista-app/app/select/page.tsx
+++ wellnista-app/app/select/page.tsx
@@ -2,24 +2,27 @@ import Link from "next/link";
 
 export default function SelectScreen() {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-secondary text-neutral font-garet text-center">
-      <h1 className="text-4xl font-magnolia text-primary mb-6">ยินดีต้อนรับเข้าสู่</h1>
-      <h1 className="text-4xl font-magnolia text-primary mb-6">Wellnista</h1>
-      <p className="mb-8 text-lg font-semibold text-neutral">
-        Choose your action below to get started.
-      </p>
-      <h1 className="text-4xl font-magnolia text-primary mb-6">ตรวจสอบโภชนาการอาหาร</h1>
-      <div className="flex flex-col gap-4">
+    <div>
+    
+    {/*Body*/}
+    <div className="text-center mt-8">
+      <h1 className="text-xl font-magnolia mb-2">ยินดีต้อนรับเข้าสู่</h1>
+      <h1 className="text-3xl font-magnolia font-bold text-[#000000]">Wellnista</h1>
+      <p className="text-sm font-magnolia text-[#000000] mt-4">Choose your action below to get start</p>
+    </div>
+      
+    <h3 className="text-center text-lg text-[#000000] mt-6">ตรวจสอบโภชนาการอาหาร</h3>
+      <div className="mt-6 flex flex-col gap-6 w-full max-w-md items-center">
         {/* ปุ่มใช้ได้ */}
         <Link href="/scan">
-          <button className="px-6 py-3 bg-primary text-secondary font-bold rounded-full hover:bg-accent transition">
+          <button className="w-80 py-4 bg-primary text-2xl text-secondary font-bold rounded-lg hover:bg-accent transition">
             สแกน บาร์โค้ด
           </button>
         </Link>
         {/* ปุ่มกำลังพัฒนา */}
         <Link href="/menu">
-          <button className="px-6 py-3 bg-primary text-secondary font-bold rounded-full hover:bg-accent transition">
-            ถ่ายรูป/พิมพ์ชื่ออาหาร
+          <button className="w-80 py-4 bg-primary text-2xl text-secondary font-bold rounded-lg hover:bg-accent transition">
+            ถ่ายรูป / พิมพ์ชื่ออาหาร
           </button>
         </Link>
       </div>
