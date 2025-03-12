e1c353c25038511ff39e21f19b80164a14b11448
omiiz9045 <kunatip.cha@gmail.com>
2025-03-12 17:11:11 +0000

    add register page

---
+++ wellnista-app/app/register/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+//import Link from "next/link";
+
+
+export default function register() {
+    return (
+    
+    <div className="grid grid-cols-6 gap-4 md:place-content-center bg-secondary text-neutral font-garet ">
+
+      <div className="col-span-6 text-center text-3xl font-magnolia text-primary ">
+        ลงทะเบียน
+      </div>
+
+      <div className="col-span-4 text-2xl font-bold text-neutral">
+        ชื่อ-นามสกุล
+      </div>
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input name
+      </div>
+
+      <div className=" col-span-4 text-2xl font-bold text-neutral">
+        ชื่อเล่น
+      </div>
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input name
+      </div>
+      
+      <div className=" col-span-6 text-2xl font-bold text-neutral">
+        โรคประจำตัว (เลือกได้มากกว่า 1)
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        เบาหวาน
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        ไต
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        ความดัน
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        ไขมัน
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        หัวใจ
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        เก๊าต์
+      </div> 
+
+      <div className=" col-span-4 text-2xl font-bold text-neutral">
+        ยาประจำตัว
+      </div>
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input name
+      </div>  
+
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        น้ำหนัก
+      </div>
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        ส่วนสูง
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input 
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        input
+      </div>  
+
+      <div className=" col-span-4 text-2xl font-bold text-neutral">
+        เลือกกิจกรรม
+      </div>
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        กิจกรรมหหนัก
+      </div>  
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        กิจกรรมปานกลาง
+      </div> 
+      <div className="py-3 col-span-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        กิจกรรมเบา
+      </div> 
+
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        BMI
+      </div>
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        IBW
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        var
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        var
+      </div> 
+
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        แคลอรี่
+      </div>
+      <div className=" col-span-3 text-2xl font-bold text-neutral">
+        คาร์บ
+      </div>
+      <div className="py-3 col-start-1 col-end-4 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+         var
+      </div>
+      <div className="py-3 col-start-4 col-end-7 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        var
+      </div> 
+
+      <div className="mt-20 py-3 col-start-2 col-end-6 bg-primary content-center text-center text-xl text-secondary font-bold rounded-md transition">
+        ลงทะเบียน
+      </div>
+    
+    </div>
+  );
+}
